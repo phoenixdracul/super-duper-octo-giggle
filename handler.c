acb7ec887ba18b84ba761fdc203aa87d1013cabc
John Blomberg <traikanossi@yahoo.com>
2014-09-26 06:16:05 -0300

    Temporary fix added.

--- handler.c
+++ handler.c
@@ -1599,8 +1599,10 @@ void extract_char( CHAR_DATA *ch, bool fPull )
 	if ( gch_prev == ch )
 		gch_prev = ch->prev;
 
-	if ( fPull && !xIS_SET(ch->act, ACT_POLYMORPHED))
+	if ( fPull /*&& !xIS_SET(ch->act, ACT_POLYMORPHED)*/ )
 		die_follower( ch );
+	// With the new ext_bv system, polymorph is getting
+	// confused somehow with the killer flag. -- Kasji
 
 	stop_fighting( ch, TRUE );
 
